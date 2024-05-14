0105526b6a3b97486a2131c076ef3ab05f20e6f6
piyushsh19 <piyush.sh19@gmail.com>
2024-05-15 00:00:11 +0530

    EMBEDDING IN ATTRIBUTE

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import Header from "./components/Header";
 import Bag from "./components/Bags";
 import Apples from "./components/Apple";
 import Pears from "./components/Pears";
+import logoss from "./logo.svg";
 
+function Logo (props){
+
+     const logoImg= <img src={logoss} ></img>
+     return logoImg
+}
 function App() {
   return (
 //     <div className="App">
@@ -10,10 +16,11 @@ function App() {
 //       <Header firstName="Any name other than Bob" />
 //     </div>
 
-    <Bag>
-        <Apples color="yellow" number="5" />
-        <Pears friend="Peter" />
-    </Bag>
+    // <Bag>
+    //     <Apples color="yellow" number="5" />
+    //     <Pears friend="Peter" />
+    // </Bag>
+    <Logo />
   );
 }
 
